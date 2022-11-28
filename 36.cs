801f4028457ec9c7afbdec959c4fe7f1fd9d4c7b
NataljaZhelinina <118520162+NataljaZhelinina@users.noreply.github.com>
2022-11-28 22:49:48 +0300

    Update 36.cs

--- 36.cs
+++ 36.cs
@@ -2,33 +2,44 @@
 //[3, 7, 23, 12] -> 19
 //[-4, -6, 89, 6] -> 0
 
-Console.WriteLine("Введите размер массива  ");
-int size = Convert.ToInt32(Console.ReadLine());
-int[] numbers = new int[size];
+int size = ReadInt("Введите размерность массива: ");
+int min = ReadInt("Введите минимальное число массива: ");
+int max = ReadInt("Введите максимальное число массива: ");
+int [] numbers = new int[size];
+
 FillArrayRandomNumbers(numbers);
-Console.WriteLine("массив: ");
 PrintArray(numbers);
-int sum = 0;
+int result = 0;
 
-for (int z = 0; z < numbers.Length; z+=2)
-    sum = sum + numbers[z];
-
-    Console.WriteLine($"всего {numbers.Length} чисел, сумма элементов cтоящих на нечётных позициях = {sum}");
-
-void FillArrayRandomNumbers(int[] numbers)
+for (int i = 0; i < numbers.Length; i++)
 {
-    for(int i = 0; i < numbers.Length; i++)
-        {
-            numbers[i] = new Random().Next(1,10);
-        }
+    if (i % 2 != 0)
+    {
+        result += numbers[i];
+    }
 }
-void PrintArray(int[] numbers)
+Console.WriteLine($"Сумма элементов нечетных позиций = {result}");
+
+// Методы
+void FillArrayRandomNumbers(int [] array) //Заполнение массива
 {
-    Console.Write("[ ");
-    for(int i = 0; i < numbers.Length; i++)
-        {
-            Console.Write(numbers[i] + " ");
-        }
-    Console.Write("]");
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max);
+    }
+}
+
+void PrintArray(int[] array) //Вывод массива на экран
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
     Console.WriteLine();
+}
+
+int ReadInt(string message)  //Функция ввода
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
 }
